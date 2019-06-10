7e3b8a5c1da6922a4cff362fde33448de71d3dd0
Anh Pham <anhphamduy@outlook.com>
2019-06-10 17:40:26 +1000

    Add back jsglue since it is an in use dependency

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask_jsglue import JSGlue
 from zenpy import Zenpy
 
 from config import config
+
+jsglue = JSGlue()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    jsglue.init_app(app)
 
     app.zenpy = Zenpy(**app.config['ZENDESK_CREDS'])
 
